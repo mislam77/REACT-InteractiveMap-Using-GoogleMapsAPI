6514c072759639809b2f1c1f7d4935f23a048d96
Md Wasiul Islam <99739954+mislam77@users.noreply.github.com>
2022-07-22 21:17:02 -0400

    Implemented the Map

--- interactive-map-project/src/App.js
+++ interactive-map-project/src/App.js
@@ -1,21 +1,22 @@
-import './App.css';
-import { } from 'react-google-maps';
+import { useMemo } from "react";
+import "./App.css";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+
+export default function Home() {
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: "AIzaSyDIsT-zUfdy3f0DPzH9G9Fh36IzZiOqiss",
+    });
+
+    if (!isLoaded) return <div>Loading...</div>;
+    return <Map />;
+}
 
 function Map() {
+    const center = useMemo(() => ({ lat: 40.757339, lng: -73.985992 }), []);
+
     return (
-        <GoogleMap
-            defaultZoom={10}
-            defaultCenter={{ lat: 40.757339, lng: -73.985992 }}
-        />
+        <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
+            <Marker position={center} />
+        </GoogleMap>
     );
 }
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Project</h1>
-    </div>
-  );
-}
-
-export default App;

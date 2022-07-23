ac45aa784390060912f7d2225a42158874199fab
Md Wasiul Islam <99739954+mislam77@users.noreply.github.com>
2022-07-22 22:11:36 -0400

    Implemented Markers

--- interactive-map-project/src/App.js
+++ interactive-map-project/src/App.js
@@ -1,8 +1,13 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import "./App.css";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import {
+    GoogleMap,
+    useLoadScript,
+    Marker
+} from "@react-google-maps/api";
 
 export default function Home() {
+
     const { isLoaded } = useLoadScript({
         googleMapsApiKey: "AIzaSyDIsT-zUfdy3f0DPzH9G9Fh36IzZiOqiss",
     });
@@ -12,11 +17,33 @@ export default function Home() {
 }
 
 function Map() {
+    const [markers, setMarkers] = useState([]);
+    const [selected, setSelected] = useState(null);
+
     const center = useMemo(() => ({ lat: 40.757339, lng: -73.985992 }), []);
 
     return (
-        <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-            <Marker position={center} />
+        <GoogleMap
+            zoom={10}
+            center={center}
+            mapContainerClassName="map-container"
+            onClick={(event) => {
+                setMarkers((current) => [
+                    ...current,
+                    {
+                        lat: event.latLng.lat(),
+                        lng: event.latLng.lng(),
+                        time: new Date(),
+                    },
+                ]);
+            }}
+        >
+            {markers.map((marker) => (
+                <Marker
+                    key={marker.time.toISOString()}
+                    position={{ lat: marker.lat, lng: marker.lng }}
+                />
+            ))}
         </GoogleMap>
     );
 }
